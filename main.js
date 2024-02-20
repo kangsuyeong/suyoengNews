7d3ebe041d6de8e420e8618ad1b60d47aeca662e
kangsuyeong <ekdh16@naver.com>
2024-02-20 19:27:26 +0900

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY =`db9569ed04774c3fb9c5a4d68a5aa546`
 let news=[]
 const getLatestNews = async ()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/v2/top-headlines?country=us&apiKey=${API_KEY}`)
     
     const response = await fetch(url)
     const data = await response.json() //json을 파일형식
