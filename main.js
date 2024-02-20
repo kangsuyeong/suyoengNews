339dd2d4751a3ec4ae458bda65d41c4cf01e1672
kangsuyeong <ekdh16@naver.com>
2024-02-20 19:18:12 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const API_KEY =`db9569ed04774c3fb9c5a4d68a5aa546`
+let news=[]
+const getLatestNews = async ()=>{
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    
+    const response = await fetch(url)
+    const data = await response.json() //json을 파일형식
+    news = data.articles
+    console.log('ddd',news)
+}
+getLatestNews()
+
+const callAPI = async()=>{
+    let url = new URL(`url 주소`)
+    let header = new Headers({헤더내용}) // 필요한 경우에만
+    let response = await fetch(url,{headers:header})
+    let data = await response.json()
+}
+
