4d85e75a531eac353cb9cafc7764c8efa27215aa
kangsuyeong <ekdh16@naver.com>
2024-02-23 19:39:11 +0900

    second commit

--- main.js
+++ main.js
@@ -3,92 +3,128 @@
 //3. 그 뉴스를 보여주기
 
 const API_KEY = `db9569ed04774c3fb9c5a4d68a5aa546`;
+let searchInput = document.getElementById("search-input")
+// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+let url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr`); //누나 API
+
+let inputArea = document.getElementById("input-area");
+let searchIcon = document.getElementById("search-icon")
 const menus = document.querySelectorAll(".menus button")
-
-
-let tabs = document.querySelectorAll(".menus button")
+const sideMenus = document.querySelectorAll(".side-menu-list button")
 let newsList = [];
-let category = ''
-let keyword = ''
 
+//메뉴 동작
 menus.forEach(menu=>
   menu.addEventListener("click",(event)=>getNewsByCategory(event))
 )
 
+sideMenus.forEach(menu=>
+  menu.addEventListener("click",(event)=>getNewsByCategory(event))
+)
+
+//엔터로 검색하기 기능
+searchInput.addEventListener("keypress",(event)=>{
+  if (window.event.keyCode == 13) {
+    searchNews()
+  }
+})
+
+
+const getNews = async ()=>{
+  try{
+    const response = await fetch(url);
+    const data = await response.json(); //json을 파일형식
+    if(response.status===200){
+      if(data.articles.length===0){
+        throw new Error("No result for this search")
+      }
+      newsList = data.articles;
+      console.log("newsList",newsList)
+      render()
+    }
+    else{
+      throw new Error(data.message)
+    }
+  }catch(error){
+    console.log("error",error.message)
+    errorRender(error.message)
+  }
+ 
+}
+
+
+
 const getLatestNews = async () => {
-  const url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?q=${keyword}&country=kr&category=${category}`); //누나 API
-  // const url = new URL(
-  //   `https://newsapi.org/v2/top-headlines?q=${keyword}&country=kr&category=${category}&apiKey=${API_KEY}`
-  // );
-  const response = await fetch(url);
-  const data = await response.json(); //json을 파일형식
-  newsList = data.articles;
-  console.log("news",newsList)
-  console.log(("category",category))
-  console.log("keyword",keyword)
-  render()
+  url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr`); //누나 API
+  getNews()
 };
+
 getLatestNews();
+
+//오류 이미지가 나왔을 때 해결방법
+const imgError = (image) => {
+	image.onerror = null; // 이미지 에러 핸들러를 중복 호출하지 않도록 이벤트 리스너를 제거합니다.
+	image.src = "https://st4.depositphotos.com/14953852/22772/v/450/depositphotos_227724992-stock-illustration-image-available-icon-flat-vector.jpg";
+};
+
+
 
 
 const render = () => {
-  const newsHTML = newsList.map(
-    (news) => 
-    `<div class="row news"> 
-    <div class="col-lg-4">
-        <img class="news-img-size"src="${news.urlToImage||"https://st4.depositphotos.com/14953852/22772/v/450/depositphotos_227724992-stock-illustration-image-available-icon-flat-vector.jpg"}">
-    </div>
-    <div class="col-lg-8">
-        <h2>${news.title}</h2>
-        <p>
-            ${news.description==null || news.description==""
-            ?"내용없음"
-            :news.description.length>200
-            ?news.description.substring(0,200)+"...":news.description}
-        </p>
-        <div>
-            ${news.source.name||"no source"} * ${moment(news.publishedAt).fromNow()}
-        </div>
-    </div>
-</div>`
-  ).join('');
-
-  document.getElementById("news-board").innerHTML = newsHTML;
+  
+    const newsHTML = newsList.map(
+      (news) => 
+      `<div class="row news"> 
+      <div class="col-lg-4">
+          <img class="news-img-size"src="${news.urlToImage}"alt="뉴스 이미지" class="news-img-size" onerror="imgError(this)">
+      </div>
+      <div class="col-lg-8">
+          <h2>${news.title}</h2>
+          <p>
+              ${news.description==null || news.description==""
+              ?"내용없음"
+              :news.description.length>200
+              ?news.description.substring(0,200)+"...":news.description}
+          </p>
+          <div>
+              ${news.source.name||"no source"} * ${moment(news.publishedAt).fromNow()}
+          </div>
+      </div>
+  </div>`
+    ).join('');
+    document.getElementById("news-board").innerHTML = newsHTML;
+    searchInput.value='' //검색어 초기화
+    inputArea.style.display = "";
+    searchIcon.style.display = "flex";
 };
 
-// const filter = (e)=>{
-//   console.log("click",e.target.id)
-//   mode = e.target.id;
-//   getLatestNews();
-// }
+const errorRender=(errorMessage)=>{
+  const errorHTML = `<div class="alert alert-danger text-center" role="alert">
+  ${errorMessage}
+</div>`
+document.getElementById("news-board").innerHTML = errorHTML;
+searchInput.value='' //검색어 초기화
+}
 
-// 기존코드
-// const getNewsByCategory= async (event)=>{
-//   const category = event.target.textContent.toLowerCase();
-//   console.log("category",category)
-//   const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
-//   const response = await fetch(url);
-//   const data = await response.json(); //json을 파일형식
-//   console.log("Ddd",data)
-
-//   newsList = data.articles;
-//   render()
-// }
 
 const getNewsByCategory= async (event)=>{
-    keyword =''
-    category = event.target.textContent.toLowerCase();
-    console.log("category",category)
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
-    // const response = await fetch(url);
-    // const data = await response.json(); //json을 파일형식
-    // console.log("Ddd",data)
-  
-    // newsList = data.articles;
-    // render()
-    getLatestNews()
-  }
+  const category = event.target.textContent.toLowerCase();
+  //url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`) //기존 API
+  url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr&category=${category}`); //누나 API
+  getNews(); //코드 리펙토리
+}
 
+const searchNews = async ()=>{
+  if(searchInput.value=="")
+    {
+        alert("검색어를 입력해주세요.");
+        return;
+    }
+  keyword = document.getElementById("search-input").value
+  //url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`) //기존 API
+  url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr&q=${keyword}`); //누나 API
+  getNews(); //코드 리펙토리
+}
 
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
@@ -99,25 +135,16 @@ const closeNav = () => {
 };
 
 const openSearchBox = () => {
-  let inputArea = document.getElementById("input-area");
-  if (inputArea.style.display === "inline") {
-    inputArea.style.display = "none";
-  } else {
-    inputArea.style.display = "inline";
+console.log(inputArea.style.display)
+  if (inputArea.style.display === "") {
+    inputArea.style.display = "flex";
+    searchIcon.style.display = "none";
   }
+
 };
 
-const searchNews = async ()=>{
-  category = '' //어디서 초기화해야할까?
-  keyword = document.getElementById("search-input").value
-  // console.log("keyword",keyword)
-  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
-  // const response = await fetch(url);
-  // const data = await response.json(); //json을 파일형식
-  // console.log("Ddd",data)
-
-  // newsList = data.articles;
-  // render()
-  getLatestNews()
-}
-
+// if (inputArea.style.display === "flex") {
+//   inputArea.style.display = "none";
+// } else {
+//   inputArea.style.display = "flex";
+// }
