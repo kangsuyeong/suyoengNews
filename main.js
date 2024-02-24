8d38d98332764a482c725c24c885ecb2dde75e98
kangsuyeong <ekdh16@naver.com>
2024-02-24 16:55:58 +0900

    second commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
 const API_KEY = `db9569ed04774c3fb9c5a4d68a5aa546`;
 let searchInput = document.getElementById("search-input")
-// let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+//let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`); // 기존 API
 let url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr`); //누나 API
 
 let inputArea = document.getElementById("input-area");
@@ -12,6 +12,13 @@ let searchIcon = document.getElementById("search-icon")
 const menus = document.querySelectorAll(".menus button")
 const sideMenus = document.querySelectorAll(".side-menu-list button")
 let newsList = [];
+
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize= 5
+
+
 
 //메뉴 동작
 menus.forEach(menu=>
@@ -32,6 +39,9 @@ searchInput.addEventListener("keypress",(event)=>{
 
 const getNews = async ()=>{
   try{
+    url.searchParams.set("page",page) // =>&page=page
+    url.searchParams.set("pageSize",pageSize)
+
     const response = await fetch(url);
     const data = await response.json(); //json을 파일형식
     if(response.status===200){
@@ -39,8 +49,11 @@ const getNews = async ()=>{
         throw new Error("No result for this search")
       }
       newsList = data.articles;
+      totalResults = data.totalResults
       console.log("newsList",newsList)
+      console.log(data)
       render()
+      paginationRender()
     }
     else{
       throw new Error(data.message)
@@ -55,6 +68,7 @@ const getNews = async ()=>{
 
 
 const getLatestNews = async () => {
+  //url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`) //기존 API
   url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr`); //누나 API
   getNews()
 };
@@ -108,6 +122,7 @@ searchInput.value='' //검색어 초기화
 
 
 const getNewsByCategory= async (event)=>{
+  page = 1;
   const category = event.target.textContent.toLowerCase();
   //url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`) //기존 API
   url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr&category=${category}`); //누나 API
@@ -118,13 +133,69 @@ const searchNews = async ()=>{
   if(searchInput.value=="")
     {
         alert("검색어를 입력해주세요.");
-        return;
+        return;W
     }
   keyword = document.getElementById("search-input").value
   //url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`) //기존 API
   url = new URL(`https://fastidious-brioche-a7bfc5.netlify.app/top-headlines?&country=kr&q=${keyword}`); //누나 API
   getNews(); //코드 리펙토리
 }
+
+const paginationRender=()=>{
+  // totalResult,
+  // page
+  // pageSize
+  // groupSize
+  //totalPages
+  const totalPages = Math.ceil(totalResults/pageSize) //totalPages = 20
+  // pageGroup
+  let pageGroup = Math.ceil(page/groupSize);
+  // lastPage
+  let lastPage = pageGroup*groupSize;
+// 마지막 페이지그룹이 그룹사이즈보다 작다? lastpage = totalpage
+if(lastPage>totalPages){
+  lastPage=totalPages
+}
+  console.log("lastPage",lastPage )
+
+  // firstPage
+  const firstPage = (lastPage-(groupSize-1))<=0?1:lastPage-(groupSize-1);
+
+  let paginationHTML=''
+  if(pageGroup!==1){ //첫번째 페이지
+    paginationHTML+= `<li class="page-item" onclick="MoveToPrePage(${lastPage})"><a class="page-link"><</a></li>`
+  }
+  for(let i=firstPage;i<=lastPage;i++){
+    paginationHTML+= `<li class="page-item ${i===page?"active":''}" onclick="MoveToPage(${i})"><a class="page-link">${i}</a></li>`
+  }
+
+  if(pageGroup!==Math.ceil(totalPages/groupSize))
+    {paginationHTML+= `<li class="page-item" onclick="MoveToNextPage(${firstPage})"><a class="page-link">></a></li>`
+  }
+
+  document.querySelector(".pagination").innerHTML=paginationHTML
+}
+
+const MoveToPage=(pageNum)=>{
+  console.log("movetopage",pageNum)
+  page = pageNum;
+  getNews()
+}
+
+const MoveToNextPage=(firstPage)=>{
+  page = firstPage + groupSize
+  console.log("page",page)
+  getNews()
+}
+
+const MoveToPrePage=(lastPage)=>{
+  page = lastPage - groupSize
+  console.log("page",page)
+  getNews()
+}
+
+
+
 
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
@@ -148,3 +219,13 @@ console.log(inputArea.style.display)
 // } else {
 //   inputArea.style.display = "flex";
 // }
+
+{/* <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav> */}
